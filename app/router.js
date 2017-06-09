7336185c19c4a2209612bc3e6cadaa248890cfc7
arlofeirman <arlofeirman@gmail.com>
2017-06-09 15:19:41 -0400

    Add Authentication
    Pulls in files for authentication for ember-auth project.
    install ember-cli-fash and ember-local-storage with ember install

--- app/router.js
+++ app/router.js
@@ -3,10 +3,13 @@ import config from './config/environment';
 
 const Router = Ember.Router.extend({
   location: config.locationType,
-  rootURL: config.rootURL
 });
 
-Router.map(function() {
+Router.map(function () {
+  this.route('sign-up');
+  this.route('sign-in');
+  this.route('change-password');
+  this.route('users');
 });
 
 export default Router;
