424ce44e7809ebdb0161192e99b2809b531cece8
arlofeirman <arlofeirman@gmail.com>
2017-06-12 11:27:01 -0400

    fix api origin for deploy

--- config/environment.js
+++ config/environment.js
@@ -52,7 +52,7 @@ module.exports = function (environment) {
   if (environment === 'production') {
     ENV.rootURL = '/free-lunch';
     ENV.locationType = 'hash';
-    ENV.apiHost = 'https://protected-fjord-91425.herokuapp.com/';
+    ENV.apiHost = 'https://protected-fjord-91425.herokuapp.com';
   }
 
   return ENV
