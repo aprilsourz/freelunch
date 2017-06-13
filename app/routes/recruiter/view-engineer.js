1a00a3f438b0d12a7bec7b60cba4041ee4968d0c
arlofeirman <arlofeirman@gmail.com>
2017-06-13 10:52:27 -0400

    Add transition to createConversation()
    Adds code to the promise chain in app/routes/recruiter/view-engineer.js
    
    When the send message form is submitted the app will transition to
    back to recruiter/engineers route.
    
    next steps: display engineers name and website above the message
    form

--- app/routes/recruiter/view-engineer.js
+++ app/routes/recruiter/view-engineer.js
@@ -4,14 +4,23 @@ import { storageFor } from 'ember-local-storage';
 export default Ember.Route.extend({
   credentials: storageFor('auth'),
   profiles: Ember.inject.service(),
+  flashMessages: Ember.inject.service(),
   model(params) {
   return this.get('store').find('engineer', params.engineer_id);
 },
-actions: {
-  createConversation(newConvo) {
-    this.get('profiles').postConversation(newConvo)
-    .then((data) => console.log(data))
+  actions: {
+    createConversation(newConvo) {
+      this.get('profiles').postConversation(newConvo)
+      .then(() => this.transitionTo('recruiter.engineers'))
+      .then(() => {
+        this.get('flashMessages')
+        .success('Lunch invite sent! Fingers crossed!');
+      })
+      .catch((error) => {
+        console.error(error);
+        this.get('flashMessages')
+        .danger('There was a problem. Please try again.');
+      });
+    }
   }
-
-}
 });
