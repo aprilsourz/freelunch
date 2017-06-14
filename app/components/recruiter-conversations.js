e1076dbc79040eb032159d6c6861d8e71262c693
arlofeirman <arlofeirman@gmail.com>
2017-06-14 17:02:29 -0400

    Fix table for in recruiter-conversation component

--- app/components/recruiter-conversations.js
+++ app/components/recruiter-conversations.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  tagName: 'table',
+  classNames: ['table'],
   actions: {
     deleteConvo(conversation) {
       this.sendAction('deleteConvo', conversation);
