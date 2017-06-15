38e0bbdfae428a55ff8590ce525b6c36c01904ac
arlofeirman <arlofeirman@gmail.com>
2017-06-15 14:30:09 -0400

    Add success message for update website
    next steps: for engineer change text of button if they have responded
    to the message

--- app/routes/recruiter/update-website.js
+++ app/routes/recruiter/update-website.js
@@ -1,17 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  flashMessages: Ember.inject.service(),
   model(params) {
     return this.get('store').find('recruiter', params.recruiter_id);
   },
   actions: {
     updateUrl(newUrl, id) {
-        this.get('store').findRecord('recruiter', id).then(function(recruiter) {
-        recruiter.set('website', newUrl);
-        recruiter.save();
-      }).then(() => {
-        this.transitionTo('recruiter');
-      });
+      return this.get('store').findRecord('recruiter', id)
+        .then(function(recruiter) {
+          recruiter.set('website', newUrl);
+          return recruiter.save();
+        }).then(() => {
+          this.transitionTo('recruiter');
+        })
+        .then(() => {
+          this.get('flashMessages').success('Updated website URL');
+        })
+        .catch(() => {
+          this.get('flashMessages').danger('There was a problem, please try again');
+        })
     }
   }
 });
