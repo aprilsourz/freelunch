091db0f7298525e7b0492f23de062a42f7d6e56e
arlofeirman <arlofeirman@gmail.com>
2017-06-26 15:06:46 -0400

    Add code to view-conversation.hbs and conversation-box.hbs
    will now display messages.

--- app/models/message.js
+++ app/models/message.js
@@ -3,6 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   senderName: DS.attr('string'),
   body: DS.attr('string'),
-
   conversationId: DS.attr('number'),
 });
--- app/routes/recruiter/view-conversation.js
+++ app/routes/recruiter/view-conversation.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
 
- this.get('store').queryRecord('message',{id: params.conversation_id})
+    return this.get('store').query('message', { id: params.conversation_id  });
 
-}
+  }
 });
