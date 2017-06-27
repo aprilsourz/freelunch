c3fd81f8b9c0561ac6aa165933dcd1856bb61165
arlofeirman <arlofeirman@gmail.com>
2017-06-26 21:12:22 -0400

    Adds this.refresh() to view-conversation.js
    next steps: reset form fields on submit

--- app/routes/recruiter/view-conversation.js
+++ app/routes/recruiter/view-conversation.js
@@ -2,17 +2,18 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
-actions: {
-  createMessage(messageParams) {
-    this.get('store')
-      .createRecord('message', messageParams).save();
-  }
-},
   model(params) {
 
   return Ember.RSVP.hash({
     messages: this.get('store').query('message', { id: params.conversation_id  }),
     conversation: this.get('store').find('conversation', params.conversation_id)
   });
+},
+actions: {
+  createMessage(messageParams) {
+    this.get('store')
+        .createRecord('message', messageParams).save()
+        .then(() => this.refresh());
   }
+},
 });
