f24dcf64e2ac6946e707d490bc6d8af95cf177d0
arlofeirman <arlofeirman@gmail.com>
2017-06-28 10:16:33 -0400

    Pulls in paper-input to replace text area

--- app/components/send-message.js
+++ app/components/send-message.js
@@ -10,11 +10,12 @@ export default Ember.Component.extend({
   credentials: storageFor('auth'),
   actions: {
     createMessage() {
+      console.log(this.get('newMessage.body'))
       this.set('newMessage.senderName', this.get('credentials.email'));
       this.set('newMessage.conversationId', this.get('conversation.id'));
       const newMessage = this.get('newMessage');
       this.sendAction('createMessage', newMessage);
       this.set('newMessage.body', null);
     }
-  }
+  },
 });
