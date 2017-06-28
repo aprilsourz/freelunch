7a704052e8fc1e8815f07b09efcb40cea63cb877
arlofeirman <arlofeirman@gmail.com>
2017-06-28 00:11:41 -0400

    Pulls in md card for recruiter conversations

--- app/components/recruiter-conversations.js
+++ app/components/recruiter-conversations.js
@@ -1,8 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  tagName: 'table',
-  classNames: ['table'],
   actions: {
     deleteConvo(conversation) {
       this.sendAction('deleteConvo', conversation);
--- app/components/recruiter-conversations/conversation.js
+++ app/components/recruiter-conversations/conversation.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-tagName: 'tr',
+
 onInit: function() {
 }.on('init'),
 actions: {
