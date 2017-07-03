dbef99dd5e8c09f4b9af4604466b2534bcfd00a2
arlofeirman <arlofeirman@gmail.com>
2017-07-03 08:35:28 -0400

    Style app/templates/recruiter/conversations.hbs

--- app/components/recruiter-conversations.js
+++ app/components/recruiter-conversations.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  tagName: 'div',
+  classNames: ['view-engineers-container'],
   actions: {
     deleteConvo(conversation) {
       this.sendAction('deleteConvo', conversation);
