5499863a86a4d30e042b4706fe9902c0d805c3fb
arlofeirman <arlofeirman@gmail.com>
2017-07-08 17:31:45 -0400

    Use new button syntax in ember 1.0

--- app/components/lunch-invites/invite.js
+++ app/components/lunch-invites/invite.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-
+  url: null,
   actions: {
     deleteConvo() {
       this.sendAction('deleteConvo', this.get('conversation'));
